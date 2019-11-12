a80580dd6b280dec899c71ed69a30b47090541ec
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-11-12 19:47:42 +0100

    Use Java 8 instead of Java 11
    Some of my projects are still using Java 8.
    It will be simpler to use the same JRE than the one needed by Gradle 😇.

--- src/test/java/fr/jcgay/gw/GradleProjectStructureTest.java
+++ src/test/java/fr/jcgay/gw/GradleProjectStructureTest.java
@@ -7,8 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GradleProjectStructureTest {
@@ -42,6 +42,6 @@ class GradleProjectStructureTest {
 
     private static void writeFakeWrapper(@TempDir Path rootDir) throws IOException {
         Path wrapper = rootDir.resolve("gradlew.sh");
-        Files.write(wrapper, List.of(""));
+        Files.write(wrapper, singletonList(""));
     }
 }
